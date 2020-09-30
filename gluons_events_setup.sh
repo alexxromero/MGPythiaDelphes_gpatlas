37707f57ba7ce13511f33867af3798ae29b80726
alexxromero <alexir2@uci.edu>
2020-09-30 13:09:45 -0700

    updated no. events

--- gluons_events_setup.sh
+++ gluons_events_setup.sh
@@ -2,26 +2,24 @@
 
 starting_dir=$(pwd)
 cards_dir="$(pwd)/cards"
-gridpack_dir="/export/home/alexir2/gridpacks_qg"
+gridpack_dir="$(HOME)/gridpacks_qg"
 pythia_card="${cards_dir}/configLHE.cmnd"
 #delphes_card="${cards_dir}/delphes_card_ATLAS.tcl"
-nEvents_run=100000
 
 # ------------------- gluons ------------------------ #
 jet_type="gluons"
 pT_range=200
 gridpack="${gridpack_dir}/${jet_type}_${pT_range}GeV_gridpack.tar.gz"
 delphes_card="${cards_dir}/delphes_card_ATLAS_${pT_range}GeV.tcl"
-nRuns=120
-seed=123
+nBatches_100k=40
+seed=4000
 event_tag="gluons_200_sept242020"
 
 python ./gen_events_slurm.py \
   -g $gridpack \
   --pythia_card $pythia_card \
   --delphes_card $delphes_card \
-  -n $nEvents_run \
-  -r $nRuns \
+  --nBatches_100k $nBatches_100k \
   --seed $seed \
   --pT_range $pT_range \
   --event_tag $event_tag
@@ -32,16 +30,15 @@ jet_type="gluons"
 pT_range=500
 gridpack="${gridpack_dir}/${jet_type}_${pT_range}GeV_gridpack.tar.gz"
 delphes_card="${cards_dir}/delphes_card_ATLAS_${pT_range}GeV.tcl"
-nRuns=140
-seed=1234
+nBatches_100k=30
+seed=5000
 event_tag="gluons_500_sept242020"
 
 python ./gen_events_slurm.py \
   -g $gridpack \
   --pythia_card $pythia_card \
   --delphes_card $delphes_card \
-  -n $nEvents_run \
-  -r $nRuns \
+  --nBatches_100k $nBatches_100k \
   --seed $seed \
   --pT_range $pT_range \
   --event_tag $event_tag
@@ -52,16 +49,15 @@ jet_type="gluons"
 pT_range=1000
 gridpack="${gridpack_dir}/${jet_type}_${pT_range}GeV_gridpack.tar.gz"
 delphes_card="${cards_dir}/delphes_card_ATLAS_${pT_range}GeV.tcl"
-nRuns=160
-seed=12345
+nBatches_100k=20
+seed=6000
 event_tag="gluons_1000_sept242020"
 
 python ./gen_events_slurm.py \
   -g $gridpack \
   --pythia_card $pythia_card \
   --delphes_card $delphes_card \
-  -n $nEvents_run \
-  -r $nRuns \
+  --nBatches_100k $nBatches_100k \
   --seed $seed \
   --pT_range $pT_range \
   --event_tag $event_tag
--- gluons_events_setup_noHad.sh
+++ gluons_events_setup_noHad.sh
@@ -2,27 +2,24 @@
 
 starting_dir=$(pwd)
 cards_dir="$(pwd)/cards"
-gridpack_dir="/export/home/alexir2/gridpacks_qg"
-#pythia_card="${cards_dir}/configLHE.cmnd"
-pythia_card="${cards_dir}/configLHE_noHad.cmnd"
+gridpack_dir="$(HOME)/gridpacks_qg"
+pythia_card="${cards_dir}/configLHE.cmnd"
 #delphes_card="${cards_dir}/delphes_card_ATLAS.tcl"
-nEvents_run=100000
 
 # ------------------- gluons ------------------------ #
 jet_type="gluons"
 pT_range=200
 gridpack="${gridpack_dir}/${jet_type}_${pT_range}GeV_gridpack.tar.gz"
 delphes_card="${cards_dir}/delphes_card_ATLAS_${pT_range}GeV.tcl"
-nRuns=140
-seed=123
+nBatches_100k=40
+seed=10000
 event_tag="gluons_200_sept242020_noHad"
 
 python ./gen_events_slurm.py \
   -g $gridpack \
   --pythia_card $pythia_card \
   --delphes_card $delphes_card \
-  -n $nEvents_run \
-  -r $nRuns \
+  --nBatches_100k $nBatches_100k \
   --seed $seed \
   --pT_range $pT_range \
   --event_tag $event_tag
@@ -33,16 +30,15 @@ jet_type="gluons"
 pT_range=500
 gridpack="${gridpack_dir}/${jet_type}_${pT_range}GeV_gridpack.tar.gz"
 delphes_card="${cards_dir}/delphes_card_ATLAS_${pT_range}GeV.tcl"
-nRuns=160
-seed=1234
+nBatches_100k=30
+seed=11000
 event_tag="gluons_500_sept242020_noHad"
 
 python ./gen_events_slurm.py \
   -g $gridpack \
   --pythia_card $pythia_card \
   --delphes_card $delphes_card \
-  -n $nEvents_run \
-  -r $nRuns \
+  --nBatches_100k $nBatches_100k \
   --seed $seed \
   --pT_range $pT_range \
   --event_tag $event_tag
@@ -53,16 +49,15 @@ jet_type="gluons"
 pT_range=1000
 gridpack="${gridpack_dir}/${jet_type}_${pT_range}GeV_gridpack.tar.gz"
 delphes_card="${cards_dir}/delphes_card_ATLAS_${pT_range}GeV.tcl"
-nRuns=180
-seed=12345
+nBatches_100k=20
+seed=12000
 event_tag="gluons_1000_sept242020_noHad"
 
 python ./gen_events_slurm.py \
   -g $gridpack \
   --pythia_card $pythia_card \
   --delphes_card $delphes_card \
-  -n $nEvents_run \
-  -r $nRuns \
+  --nBatches_100k $nBatches_100k \
   --seed $seed \
   --pT_range $pT_range \
   --event_tag $event_tag
